58488b035a66a36d56f3a40f31fae4945a5581ad
ravikushwaha020596-cell <ravikushwaha020596@gmail.com>
2026-07-27 22:34:59 +0530

    Fix MongoDB Atlas connection

--- backend/database/db.js
+++ backend/database/db.js
@@ -1,12 +1,12 @@
 import mongoose from "mongoose";
 
-const connectDB = async()=>{
-    try {
-        await mongoose.connect(`${process.env.MONGO_URL}/EKART-RAVI`)
-        console.log('MongoDB connect Successfully');
-    } catch (error) {
-        console.log("MongoDB connection failed:", error);
-    }
-}
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URL);
+    console.log("MongoDB connect Successfully");
+  } catch (error) {
+    console.log("MongoDB connection failed:", error);
+  }
+};
 
-export default connectDB
+export default connectDB;

5ec8702280ed05b5852ae21f8a058f315c541f16
ravikushwaha020596-cell <ravikushwaha020596@gmail.com>
2026-07-31 12:30:42 +0530

    Fix gmail smtp for render

--- backend/emailVerifiy/verifyEmail.js
+++ backend/emailVerifiy/verifyEmail.js
@@ -2,51 +2,50 @@ import nodemailer from "nodemailer";
 import "dotenv/config.js";
 
 export const verifyEmail = async (token, email) => {
-   try {
+  try {
+    const transporter = nodemailer.createTransport({
+      host: "smtp.gmail.com",
+      port: 587,
+      secure: false,
+      family: 4,
+      auth: {
+        user: process.env.MAIL_USER,
+        pass: process.env.MAIL_PASS,
+      },
+    });
 
-  const transporter = nodemailer.createTransport({
-    service: "gmail",
-    auth: {
-      user: process.env.MAIL_USER,
-      pass: process.env.MAIL_PASS,
-    },
-     
-  });
+    const verifyUrl = `${process.env.CLIENT_URL}/verify/${token}`;
 
-  const verifyUrl = `${process.env.CLIENT_URL}/verify/${token}`;
+    const mailConfigurations = {
+      from: process.env.MAIL_USER,
+      to: email,
+      subject: "Email Verification",
 
-
-  
-  const mailConfigurations = {
-    from: process.env.MAIL_USER,
-    to: email,
-
-    subject: "Email Verification",
-
-    html: `<h2>Welcome to E-Commerce</h2>
+      html: `
+        <h2>Welcome to E-Commerce</h2>
         <p>Click the button below to verify your email.</p>
 
         <a href="${verifyUrl}"
-           style="
-             display:inline-block;
-             padding:12px 20px;
-             background:#2563eb;
-             color:#fff;
-             text-decoration:none;
-             border-radius:6px;
-           ">
-           Verify Email
+        style="
+          display:inline-block;
+          padding:12px 20px;
+          background:#2563eb;
+          color:#fff;
+          text-decoration:none;
+          border-radius:6px;
+        ">
+        Verify Email
         </a>
 
         <p>If the button doesn't work, copy and paste this link:</p>
-
         <p>${verifyUrl}</p>
       `,
     };
 
     await transporter.sendMail(mailConfigurations);
 
-  console.log("Verification Email Sent Successfully");
+    console.log("Verification Email Sent Successfully");
+
   } catch (error) {
     console.error("Error Sending Verification Email:", error.message);
     throw error;

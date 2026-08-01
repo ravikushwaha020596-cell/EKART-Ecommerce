26e9a2a52dc5243fed76b1a69ef2b3012eb97ab8
ravikushwaha020596-cell <ravikushwaha020596@gmail.com>
2026-08-01 15:46:40 +0530

    update email service

--- backend/emailVerifiy/sendOTPMail.js
+++ backend/emailVerifiy/sendOTPMail.js
@@ -13,13 +13,7 @@ const transporter = nodemailer.createTransport({
   },
 });
 
-transporter.verify((error, success) => {
-  if (error) {
-    console.error("SMTP VERIFY ERROR:", error);
-  } else {
-    console.log("SMTP Server is ready");
-  }
-});
+
 
 export const sendOTPMail = async (otp, email) => {
 

433895b7b280ca948a2bb50f2c266d08d2a5455c
ravikushwaha020596-cell <ravikushwaha020596@gmail.com>
2026-08-02 10:47:39 +0530

    debug resend email response

--- backend/emailVerifiy/verifyEmail.js
+++ backend/emailVerifiy/verifyEmail.js
@@ -3,13 +3,15 @@ import "dotenv/config";
 
 const resend = new Resend(process.env.RESEND_API_KEY);
 
-
 export const verifyEmail = async (token, email) => {
   try {
 
     const verifyUrl = `${process.env.CLIENT_URL}/verify/${token}`;
 
-    await resend.emails.send({
+    console.log("FINAL VERIFY URL:", verifyUrl);
+    console.log("SEND TO:", email);
+
+    const result = await resend.emails.send({
       from: "Ekart <onboarding@resend.dev>",
       to: email,
       subject: "Email Verification",
@@ -19,35 +21,23 @@ export const verifyEmail = async (token, email) => {
 
         <p>Click below to verify your email</p>
 
-        <a href="${verifyUrl}"
-        style="
-          padding:10px 20px;
-          background:#ec4899;
-          color:white;
-          text-decoration:none;
-          border-radius:5px;
-        ">
+        <a href="${verifyUrl}">
           Verify Email
         </a>
 
-        <br><br>
+        <br/><br/>
 
-        <p>
-        If button doesn't work, copy this link:
-        </p>
-
-        <a href="${verifyUrl}">
-          ${verifyUrl}
-        </a>
+        <p>${verifyUrl}</p>
       `,
     });
 
+    console.log("RESEND RESULT:", result);
 
     console.log("Verification Email Sent Successfully");
 
-  } catch (error) {
+  } catch(error) {
 
-    console.log("VERIFY EMAIL ERROR:", error);
+    console.log("MAIL ERROR:", error);
 
     throw error;
   }
